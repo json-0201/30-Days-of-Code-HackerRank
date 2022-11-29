b943b6e5a663e67c8a95fcb858fd928e17be75b4
json-0201 <106159207+json-0201@users.noreply.github.com>
2022-11-29 12:26:59 -0800

    Create day_10.py

---
+++ day_10.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+ 
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    
+binary = []
+
+while int(n/2) != 0:
+  binary.append(n%2)
+  n=int(n/2)
+binary.append(1)
+
+if n==0:
+  binary = [0]
+
+binary.reverse()
+binary="".join(list(map(str,binary)))
+
+consecutive=[num[0] for num in re.findall(r'(([01])\2+)', binary)]
+
+if not consecutive:
+  print(1)
+else:
+  print(len(max(consecutive)))
